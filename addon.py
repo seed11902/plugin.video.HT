c940bdd52839e5a92a2a65160eedb141650cafb5
seed11902 <seed11902@ymail.com>
2016-10-12 21:21:06 +0800

    Update addon.py
    adddir('Bravest Warriors',{'mode': 'folder','Type': 'hornydragon',  'Url': 'http://hdx3.blogspot.com/search/label/Bravest%20Warriors?max-results=200'},'')

--- addon.py
+++ addon.py
@@ -134,6 +134,7 @@ elif mode[0] == 'hdx3':
         xbmcplugin.addSortMethod(addon_handle, sortMethod=xbmcplugin.SORT_METHOD_TITLE)
         xbmcplugin.endOfDirectory(addon_handle)
 elif mode[0] == 'hornydragon':
+        adddir('Bravest Warriors',{'mode': 'folder','Type': 'hornydragon',  'Url': 'http://hdx3.blogspot.com/search/label/Bravest%20Warriors?max-results=200'},'')
         adddir('Angry Video Game Nerd',{'mode': 'folder','Type': 'hornydragon', 'Url': 'http://hornydragon.blogspot.com/search/label/AVGN?max-results=200'},'')
         adddir('Board James',{'mode': 'folder','Type': 'hornydragon', 'Url': 'http://hornydragon.blogspot.com/search/label/Board%20James?max-results=200'},'')
         adddir('PewDiePie',{'mode': 'folder','Type': 'hornydragon', 'Url': 'http://hornydragon.blogspot.com/search/label/PewDiePie?max-results=200'},'')
